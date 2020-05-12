7574480530ab579ff410d690eefd82087521633e
Neo Genesis <genesis_neo@live.com>
2020-05-12 17:02:36 +0400

    Update vercel system environment variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
-// zeit system environment variables
-const processEnvNowGithubCommitSha = process.env.NOW_GITHUB_COMMIT_SHA;
+// vercel system environment variables
+const processEnvNowGithubCommitSha = process.env.VERCEL_GITHUB_COMMIT_SHA;
 // custom environment variables
 const processEnvVersionHash = process.env.VERSION_HASH;
 const versionHash = processEnvNowGithubCommitSha
